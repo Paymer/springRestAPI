f51e8ed72d3ffe92dc5c8f83580d40038dd94ed7
Paymer <p.aymerich.f@gmail.com>
2023-06-29 23:40:25 +0200

    first commit

---
+++ src/main/java/com/restApi/springRestAPI/Greeting.java
@@ -0,0 +1,3 @@
+package com.restApi.springRestAPI;
+
+public record Greeting(long id, String content) { }
---
+++ src/test/java/com/restApi/springRestAPI/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restApi.springRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
